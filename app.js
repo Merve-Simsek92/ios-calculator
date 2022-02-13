dd24f1e198d182fb099c5843f340618ca111dadf
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-02-13 15:54:43 +0300

    çalışma

---
+++ app.js
@@ -0,0 +1,24 @@
+const acE1=document.querySelector(".content2");
+const pmE1=document.querySelector(".content3");
+const percent=document.querySelector(".concent4");
+
+const additionE1=document.querySelector(".content16");
+const subtractionE1=document.querySelector(".content12");
+const divisionE1=document.querySelector(".content5");
+const multiplicationE1=document.querySelector(".content20");
+const equalE1=document.querySelector(".content19");
+
+
+const decimalE1=document.querySelector(".content18");
+const number01=document.querySelector(".content13");
+const number02=document.querySelector(".content14");
+const number03=document.querySelector(".content15");
+const number04=document.querySelector(".content9");
+const number05=document.querySelector(".content10");
+const number06=document.querySelector(".content11");
+const number07=document.querySelector(".content6");
+const number08=document.querySelector(".content7");
+const number09=document.querySelector(".content8");
+
+
+
